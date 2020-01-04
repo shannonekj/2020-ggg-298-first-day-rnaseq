16e40c341190dc2b0065ec8f9fb758c005968572
C. Titus Brown <titus@idyll.org>
2020-01-03 17:36:16 -0800

    set binder URL; add install stuff for rmarkdown

--- install.R
+++ install.R
@@ -2,3 +2,6 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
 BiocManager::install("edgeR", update=FALSE, ask=FALSE)
+
+install.packages(c("highr", "markdown", "stringr", "yaml"))
+install.packages(c("caTools", "bitops", "knitr", "rprojroot", "rmarkdown"))
